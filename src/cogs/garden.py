18525abb8f6740530caa4caeb38ad96ba6bf468b
nickshouse <nickshouse@proton.me>
2022-12-01 14:45:38 -0600

    Setup classes for chao creation

---
+++ src/cogs/garden.py
@@ -0,0 +1,16 @@
+from discord import app_commands
+from discord.ext import commands
+
+# all cogs inherit from this base class
+class Garden(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot # adding a bot attribute for easier access
+
+    # doing something when the cog gets loaded/unloaded
+    async def cog_load(self):
+        print(f"{self.__class__.__name__} loaded!")
+    async def cog_unload(self):
+        print(f"{self.__class__.__name__} unloaded!")
+
+async def setup(bot):
+    await bot.add_cog(Garden(bot=bot))
